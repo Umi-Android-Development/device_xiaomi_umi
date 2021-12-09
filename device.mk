79bb2ab42227ec2f2f610669620118edcd6e05ff
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 20:24:28 +0100

    umi: Namespace the tree
    Change-Id: I4642cbdff50e5f43e35dffb726565a829a8ccada

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from firmware repo
 $(call inherit-product, vendor/xiaomi-firmware/umi/firmware.mk)
 
